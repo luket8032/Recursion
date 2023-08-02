fc9cef352bb059240cc5e8a937a8a4a9ee476983
Luke <luket8032@hotmail.com>
2023-08-02 17:27:12 -0500

    Add function for fibonacci using recursion

--- Assignment_1/fib.js
+++ Assignment_1/fib.js
@@ -1,8 +1,8 @@
-// function "fibs(n)" returns an array of numbers representing the fibonacci sequence.
+// function "fibs(n)" returns an array of numbers representing the fibonacci sequence using iteration
 // n represents the length of the returned array
 
 function fibs(n) {
-    if (n < 1) return "please select a value greater than 1"; // check for lengths less than 1
+    if (n < 1 || isNaN(n)) return "please select a value greater than 1"; // check for lengths less than 1 or NaN
     if (n === 1) return [0];
     if (n === 2) return [0, 1];
 
@@ -14,9 +14,23 @@ function fibs(n) {
     return arr
 }
 
+// function "fibsRec(n)" returns array of numbers of fibonacci sequence using recursion
+// n represents the length of the returned array
+
+function fibsRec(n ,fibArr = [0, 1]) {
+    if (n < 1 || isNaN(n)) return "please select a value greater than 1"; // check for lengths less than 1 or NaN
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+    if (fibArr.length >= n) return fibArr;
+    return fibsRec(n, [...fibArr, (Number(fibArr.slice(-1)) + Number(fibArr.slice(-2, -1))) ])
+}
+
+
 console.log(fibs(0)) // returns "please select a value greater than 1"
 console.log(fibs(8)) // returns [0, 1, 1, 2, 3, 5, 8, 13]
 
+console.log(fibsRec(0)) // returns "please select a value greater than 1"
+console.log(fibsRec(6)) // returns [0, 1, 1, 2, 3, 5]
 
 
 
